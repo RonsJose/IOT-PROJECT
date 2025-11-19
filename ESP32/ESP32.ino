b7b4219aeddbddd54ee469f4685b2f7debef8152
RonsJose <g00464215@atu.ie>
2025-11-19 16:47:09 +0000

    Updated smtp server
    Doesnt havent embeded message so it takes less space

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -8,6 +8,7 @@
 #include <DFRobot_DHT11.h>
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
+#include <ESP_Mail_Client.h>
 
 AsyncWebServer server(80);
 
@@ -22,6 +23,83 @@ const int TRIG_PIN = 25;
 const int ECHO_PIN = 14;
 long duration;
 float cms, inches;
+#define emailSenderAccount    "esp32rj@gmail.com"
+#define emailSenderPassword   "bcho nulq vuvz rixe"
+#define smtpServer            "smtp.gmail.com"
+#define smtpServerPort        465
+#define emailSubject          "ESP32-CAM Photo Captured"
+#define emailRecipient        "g004642115@atu.ie"
+
+SMTPSession smtp;
+
+void smtpCallback(SMTP_Status status);
+
+void smtpCallback(SMTP_Status status){
+  /* Print the current status */
+  Serial.println(status.info());
+
+  /* Print the sending result */
+  if (status.success())
+  {
+    Serial.println("----------------");
+    Serial.printf("Message sent success: %d\n", status.completedCount());
+    Serial.printf("Message sent failled: %d\n", status.failedCount());
+    Serial.println("----------------\n");
+    struct tm dt;
+
+    for (size_t i = 0; i < smtp.sendingResult.size(); i++){
+      /* Get the result item */
+      SMTP_Result result = smtp.sendingResult.getItem(i);
+      time_t ts = (time_t)result.timestamp;
+      localtime_r(&ts, &dt);
+
+      ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
+      ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
+      ESP_MAIL_PRINTF("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
+      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients.c_str());
+      ESP_MAIL_PRINTF("Subject: %s\n", result.subject.c_str());
+    }
+    Serial.println("----------------\n");
+
+   // You need to clear sending result as the memory usage will grow up.
+   smtp.sendingResult.clear();
+  }
+}
+
+void sendEmail()
+{
+smtp.debug(1);
+smtp.callback(smtpCallback);
+Session_Config config;
+  config.time.ntp_server = F("pool.ntp.org,time.nist.gov");
+  config.time.gmt_offset = 0;
+  config.time.day_light_offset = 1;
+  config.server.host_name = smtpServer;
+  config.server.port = smtpServerPort;
+  config.login.email = emailSenderAccount;
+  config.login.password = emailSenderPassword;
+  config.login.user_domain = "";
+
+  SMTP_Message message;
+  message.enable.chunking = true;
+  message.sender.name = "ESP32-CAM";
+  message.sender.email = emailSenderAccount;
+  message.subject = emailSubject;
+  message.addRecipient("Sara", emailRecipient);
+
+  String htmlMsg = "<h2>Photo captured with ESP32-CAM and attached in this email.</h2>";
+  message.html.content = htmlMsg.c_str();
+  message.html.charSet = "utf-8";
+  message.html.transfer_encoding = Content_Transfer_Encoding::enc_qp;
+
+  message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_normal;
+  message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;
+  if (!smtp.connect(&config))
+    return;
+
+  if (!MailClient.sendMail(&smtp, &message, true))
+    Serial.println("Error sending Email, " + smtp.errorReason());
+}
 
 String getTemp() {
    DHT.read(DHT11_PIN);
@@ -47,7 +125,6 @@ String getHumid(){
   }
 else 
 {
-  DHT.read(DHT11_PIN);
   Serial.print("  humi:");
   Serial.println(DHT.humidity);
   return String(DHT.humidity) + " %";
@@ -183,6 +260,7 @@ void setup(void) {
 
   server.begin();
   Serial.println("HTTP server started");
+  sendEmail();
 }
 
 void loop(void) {
