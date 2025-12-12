2ebb175620680da73c08ea944a2f01a9530a87b8
RonsJose <g00464215@atu.ie>
2025-12-12 09:59:31 +0000

    Added lock, so both email and lock work off of same esp32

--- ESP32_EMAIL/ESP32_EMAIL.ino
+++ ESP32_EMAIL/ESP32_EMAIL.ino
@@ -2,14 +2,25 @@
 #include <HTTPClient.h>
 #include "cred.h"
 #include <PubSubClient.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <ESP32Servo.h>
+
+#define SS_PIN 5
+#define RST_PIN 0
 
 const char *mqtt_broker = "165.22.122.17";
 const char *topic1 = "sensor/alcohol";
 const int mqtt_port = 1883;
 
+Servo motor;
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+const int pin = 14;
+bool LockCheck = true;
 String al;
 
 void callback(char *topic, byte *payload, unsigned int length) {
@@ -29,6 +40,12 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) delay(500);
   Serial.println("WiFi connected!");
 
+  SPI.begin();
+  mfrc522.PCD_Init();
+  motor.setPeriodHertz(50);
+  motor.attach(pin, 500, 2400);
+  motor.write(0);
+
   client.setCallback(callback);
   client.setServer(mqtt_broker, mqtt_port);
 }
@@ -39,7 +56,7 @@ void loop() {
     client_id += String(WiFi.macAddress());
     Serial.println("Connecting to mqtt server\n");
     if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
-      Serial.println("Connected to MQTT server"); 
+      Serial.println("Connected to MQTT server");
       client.subscribe(topic1);
     } else {
       Serial.println("Failed to connect ");
@@ -49,9 +66,45 @@ void loop() {
   }
   client.loop();
 
-  if(al=="Alcohol level: High")
-  {
-    sendMail("Alert",al);
+  if (al == "Alcohol level: High") {
+    sendMail("Alert", al);
+  }
+
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  Serial.print("Card ID: ");
+  String cardID = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    cardID.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    cardID.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+
+  cardID.toUpperCase();
+  if (cardID.substring(1) == "B7 C3 B0 01") {
+    Serial.println("Access granted");
+    Serial.println();
+    if (LockCheck) {
+      unlock();
+      LockCheck = false;
+    } else {
+      lock();
+      LockCheck = true;
+    }
+    delay(2000);
+  } else {
+    Serial.println("Access denied. Unauthorized card");
+    lock();
+    LockCheck = true;
+    delay(2000);
   }
 }
 
@@ -82,4 +135,16 @@ void sendMail(String subject, String body) {
   }
 
   http.end();
+}
+
+void unlock() {
+  Serial.println("Unlocking");
+  motor.write(90);
+  delay(500);
+}
+
+void lock() {
+  Serial.println("Locking");
+  motor.write(0);
+  delay(500);
 }
