724aaf1d0d26733d0ef0f3454cb46e893b9ba82f
RonsJose <g00464215@atu.ie>
2025-12-12 09:47:39 +0000

    Added mqtt and emailing function when alcohol level is high

--- ESP32_EMAIL/ESP32_EMAIL.ino
+++ ESP32_EMAIL/ESP32_EMAIL.ino
@@ -1,6 +1,26 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include "cred.h"
+#include <PubSubClient.h>
+
+const char *mqtt_broker = "165.22.122.17";
+const char *topic1 = "sensor/alcohol";
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+String al;
+
+void callback(char *topic, byte *payload, unsigned int length) {
+
+  if (strcmp(topic, topic1) == 0) {
+    al = "";
+    for (int i = 0; i < length; i++) {
+      al += ((char)payload[i]);
+    }
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -9,10 +29,31 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) delay(500);
   Serial.println("WiFi connected!");
 
-  sendMail("Hello", "Test");
+  client.setCallback(callback);
+  client.setServer(mqtt_broker, mqtt_port);
 }
 
-void loop() {}
+void loop() {
+  if (!client.connected()) {
+    String client_id = "esp32-client-";
+    client_id += String(WiFi.macAddress());
+    Serial.println("Connecting to mqtt server\n");
+    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("Connected to MQTT server"); 
+      client.subscribe(topic1);
+    } else {
+      Serial.println("Failed to connect ");
+      Serial.print(client.state());
+      delay(1000);
+    }
+  }
+  client.loop();
+
+  if(al=="Alcohol level: High")
+  {
+    sendMail("Alert",al);
+  }
+}
 
 void sendMail(String subject, String body) {
   if (WiFi.status() != WL_CONNECTED) return;
