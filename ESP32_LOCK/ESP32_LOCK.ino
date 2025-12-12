57470f67b8a1f0f1206d8901d4ff823fe8f5bee8
RonsJose <g00464215@atu.ie>
2025-12-12 01:11:26 +0000

    Merged rfid and servo motor code together to create a key and lock system

--- ESP32_LOCK/ESP32_LOCK.ino
+++ ESP32_LOCK/ESP32_LOCK.ino
@@ -1,19 +1,26 @@
 #include <SPI.h>
 #include <MFRC522.h>
-
+#include <ESP32Servo.h>
 
 #define SS_PIN 5
 #define RST_PIN 0
 
+Servo motor;
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+const int pin = 14;
+bool LockCheck = true;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   SPI.begin();
   mfrc522.PCD_Init();
-  Serial.println("Hold the card close to the reader...");
+  Serial.println("Hold the card close to the reader");
   Serial.println();
+  motor.setPeriodHertz(50);
+  motor.attach(pin, 500, 2400);
+  motor.write(0);
   delay(500);
 }
 
@@ -39,11 +46,33 @@ void loop() {
 
   cardID.toUpperCase();
   if (cardID.substring(1) == "B7 C3 B0 01") {
-    Serial.println("Access granted. Welcome!");
+    Serial.println("Access granted");
     Serial.println();
+    if (LockCheck) {
+        unlock();
+        LockCheck = false;
+      }
+    else {
+      lock();
+      LockCheck = true;
+    }
     delay(2000);
   } else {
-    Serial.println("Access denied. Unauthorized card.");
+    Serial.println("Access denied. Unauthorized card");
+    lock();
+    LockCheck = true;
     delay(2000);
   }
+}
+
+void unlock() {
+  Serial.println("Unlocking");
+  motor.write(90);
+  delay(500);
+}
+
+void lock() {
+  Serial.println("Locking");
+  motor.write(0);
+  delay(500);
 }
