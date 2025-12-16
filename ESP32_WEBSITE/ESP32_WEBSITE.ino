babd31f7a250efbb6d9efc8af7549e9f40800757
RonsJose <g00464215@atu.ie>
2025-12-16 19:40:23 +0000

    Added comments to the code for better understanding

--- ESP32_WEBSITE/ESP32_WEBSITE.ino
+++ ESP32_WEBSITE/ESP32_WEBSITE.ino
@@ -1,3 +1,11 @@
+/*
+This code is for my IoT project the Smart System Integration for Automobiles
+This is run on an ESP32
+This one is responsible for the webserver and uploading data to thingspeak
+It hosts the async webserver and sends the sensor data to thingspeak 
+*/
+
+//Libraries
 #include <WiFi.h>
 #include <NetworkClient.h>
 #include <AsyncTCP.h>
@@ -8,16 +16,22 @@
 #include "ThingSpeak.h"
 #include "cred.h"
 
+//Webserver port
 AsyncWebServer server(80);
 
+//Thingspeak Config
 unsigned long channelNum = 3151130;
 #define UPLOAD_PERIOD 20000
-unsigned long uploadLast = 0;
+unsigned long uploadLast = 0;#
+
+//MQTT client
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+//Sensor data
 String Temp, Dist, Humidity, Blood, Heart, Long, Lat, ip, alcohol, address;
 
+//MQTT config
 const char *mqtt_broker = "165.22.122.17";
 const char *topic1 = "sensor/distance";
 const char *topic2 = "sensor/temperature";
@@ -31,6 +45,7 @@ const char *topic9 = "sensor/alcohol";
 const char *topic10 = "gps/address";
 const int mqtt_port = 1883;
 
+//Loads template and replaces placeholder values 
 void handleRoot(AsyncWebServerRequest *request) {
   String page = String(homePage);
   page.replace("%TEMPERATURE%", Temp.c_str());
@@ -44,6 +59,7 @@ void handleRoot(AsyncWebServerRequest *request) {
   request->send(200, "text/html", page);
 }
 
+//Handles errors
 void handleNotFound(AsyncWebServerRequest *request) {
   String message = "File Not Found\n\n";
   message += "URI: ";
@@ -60,6 +76,7 @@ void handleNotFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", message);
 }
 
+//Called whenever a topic receives data and updates the variable for that topic
 void callback(char *topic, byte *payload, unsigned int length) {
 
   if (strcmp(topic, topic1) == 0) {
@@ -135,6 +152,7 @@ void callback(char *topic, byte *payload, unsigned int length) {
 
 void setup() {
   // put your setup code here, to run once:
+  //Connect to wifi, start serial, starts thingspeak client, configs mqtt client and callback, and configs the HTTP routes
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -150,13 +168,16 @@ void setup() {
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
+  
+  //MDNS for when at home for wifi
   if (MDNS.begin("iot")) {
     Serial.println("MDNS responder started");
   }
 
+  //Loads the main webpage
   server.on("/", HTTP_GET, handleRoot);
 
+  //HTTP GET endpoints that send back the current value of whatever sensor that is requested
   server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "text/plain", Temp.c_str());
   });
@@ -197,17 +218,21 @@ void setup() {
     request->send(200, "text/plain", address.c_str());
   });
 
+  //Handles server not working
   server.onNotFound(handleNotFound);
 
+  //starts server
   server.begin();
   Serial.println("HTTP server started");
 
+  //mqtt broker and callback config
   client.setCallback(callback);
   client.setServer(mqtt_broker, mqtt_port);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //Connects to mqtt broker and subscribes to all the topics 
   if (!client.connected()) {
     String client_id = "esp32-client-";
     client_id += String(WiFi.macAddress());
@@ -233,10 +258,11 @@ void loop() {
   }
   client.loop();
 
+  //Uploads data to thingspeak every 20 seconds
   unsigned long now = millis();
   if (now - uploadLast >= UPLOAD_PERIOD) {
 
-    if (Temp == "" || Humidity == "" || Dist == "" || Heart == "" || Blood == "") {
+    if (Temp == "" || Humidity == "" || Dist == "" || Heart == "" || Blood == "") { //Skips uploading if one of the variables is empty to avoid errors
       Serial.println("skip");
       uploadLast = now;
       return;
