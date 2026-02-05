d076fd71416e06702c4dfb3e34d6de879bd3ce90
RonsJose <g00464215@atu.ie>
2026-02-05 09:08:17 +0000

    Added Area for lock in html

--- website/app.py
+++ website/app.py
@@ -32,4 +32,4 @@ def graphs():
     return render_template("graphs.html")
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=8000)
+    socketio.run(app, host="0.0.0.0", port=5000)
