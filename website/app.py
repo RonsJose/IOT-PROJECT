5ecabb07f38b784491a39355a086d832260624e5
RonsJose <g00464215@atu.ie>
2026-03-11 21:05:45 +0000

    Uncommented enabling databases

--- website/app.py
+++ website/app.py
@@ -185,8 +185,8 @@ def get_users():
 
     return jsonify(result)
 
-#with app.app_context():
-#    db.create_all()
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=8000)
